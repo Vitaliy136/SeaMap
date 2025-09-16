65b16bb11fa7017cdcfcbabfaf6adb93fd29a09b
Vitaliy136 <vitaly136@gmail.com>
2025-09-16 22:35:28 +0300

    Remove SeaMap component from App.vue
    Removed import and usage of SeaMap component.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-  import SeaMap from './components/SeaMap.vue';
   import LF from './components/LF.vue';
   import PWABadge from './components/PWABadge.vue';
 </script>
 
 <template>
-  <!-- <SeaMap/> -->
   <LF/>
   <PWABadge />
 </template>
